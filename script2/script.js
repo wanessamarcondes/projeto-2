260a51ecbde709fed8e13a5fe5406d27b9d507fe
wanessamarcondes <>
2018-09-27 14:28:44 -0300

    aaaaa

--- script2/script.js
+++ script2/script.js
@@ -2,6 +2,25 @@ const addButton = document.querySelector(".new-task__button")
 const inputTask = document.getElementById("input-task")
 const allTasks = document.querySelector(".all-tasks")
 
+// DRAG AND DROP
+
+//function dragstart_handler(ev) {
+//    ev.dataTransfer.setData("text/plain", ev.target.id);
+ //   ev.dropEffect = "move";
+ //   }
+
+//function dragover_handler(ev) {
+//    ev.preventDefault();
+//    ev.dataTransfer.dropEffect = "move"
+//    }
+
+//function drop_handler(ev) {
+ //   ev.preventDefault();
+ //   var data = ev.dataTransfer.getData("text");
+ //   //ev.target.appendChild(document.getElementById//(data));
+ //   }
+
+///////////////////////////////
 addButton.addEventListener("click", function(event){
     event.preventDefault();
 
@@ -11,25 +30,24 @@ addButton.addEventListener("click", function(event){
         return false;
     }
 
-    const newTask = document.createElement("div");
-    newTask.className = "task";
-    newTask.setAttribute("id", "task");
-    newTask.setAttribute("draggable", "true");
-    newTask.setAttribute("ondragstart", "dragstart_handler(event);")
-
+    const newTask = document.createElement("<div class="task">
+    <h2 class="task__name">`${inputTask.value}`</h2>
+    <button class="button remove-task__button">x</button></div>")
+    newTask.setAttribute("id", "new-task")
+    newTask.setAttribute("draggable", true);
     const newTaskName = document.createElement("h2");
-    newTaskName.className = "task__name";
-    newTaskName.innerHTML = inputTask.value;
-
     const removeButton = document.createElement("button");
+
+    newTask.className = "task";
+    newTaskName.className = "task__name";
     removeButton.className = "remove-task__button";
-    removeButton.innerHTML = "x";    
+
+    newTaskName.innerHTML = inputTask.value;
+    removeButton.innerHTML = "x";
 
     newTask.appendChild(newTaskName);
     newTask.appendChild(removeButton);
     allTasks.appendChild(newTask);
-
-    newTask.setAttribute("draggable", "true");
 
     const footer = document.querySelector(".footer")
     const checkAllTasks = document.querySelector(".all-tasks-done__button")
@@ -58,12 +76,21 @@ addButton.addEventListener("click", function(event){
     //checkAllTasks.addEventListener("click", function(event){
         //event.preventDefault();
         //newTaskName.className = "task__name_done";
+        //checkAllTasks.innerHTML = "Desfazer";
+        //checkAllTasks.addEventListener("click", function(event){
+            //newTaskName.className = "task__name"
+        //})
+    //})
+
+    checkAllTasks.addEventListener("click", function(event){
+        event.preventDefault();
+        newTaskName.className = "task__name_done";
         
         //checkAllTasks.addEventListener("click", function(event){
             //event.preventDefault();
             //newTaskName.className = "task__name"
         //})
-    //})
+    })
 
     clearAllTasks.addEventListener("click", function(event){
         event.preventDefault();
@@ -74,23 +101,3 @@ addButton.addEventListener("click", function(event){
     inputTask.value = "";
     inputTask.focus();
 })
-
-// DRAG AND DROP
-
-function dragstart_handler(ev) {
-    ev.dataTransfer.setData("text/plain", ev.target.id);
-    ev.dropEffect = "move";
-    }
-
-function dragover_handler(ev) {
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = "move"
-    }
-
-function drop_handler(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    }
-
-///////////////////////////////
